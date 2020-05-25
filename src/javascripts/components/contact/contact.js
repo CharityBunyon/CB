2fbac93f83db103f7f1831c06e9f8f85db4772ac
Charity Bunyon <cseymou5@gmail.com>
2020-05-24 21:18:48 -0500

    finalized contact area

--- src/javascripts/components/contact/contact.js
+++ src/javascripts/components/contact/contact.js
@@ -1,39 +1,32 @@
 import './contact.scss';
+// import axios from 'axios';
 import utilities from '../../helpers/utilities';
+// mport airplane from '../../../images/airplane.svg';
+// import apiKeys from '../apiKeys.json';
 
-import airplane from '../../../images/airplane.svg';
-
+// const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const buildContactSection = () => {
   const domString = `
-    <div class="contact-wrapper d-flex row flex-wrap"">
-      <div class="contact-image col">
-        <img class="icon" src="${airplane}" alt="email">
-      </div>
+    <div class="contact-wrapper d-flex row flex-wrap">
 
-      <div class="contact-form col">
-        <h3 class="form-title">Contact Me</h3>
-        <form>
-        <div class="form-group">
-            <input type="text" class="form-control sections" id="" name="name" aria-describedby="emailHelp" placeholder="Name">
-          </div>
+      <div class="contact-form text-center col">
+        <h2 class="text-center contact-title">Contact Me</h2>
+        <li class="contact-para contact-links">Want to connect? Please feel free to reach out to me!</li>
+        
+        <li class="contact-links"><a href="mailto:charitycbunyon@gmail.com" target="_blank"><i class="far fa-paper-plane contact-icons"></i>Email Me</a></li>
 
-          <div class="form-group">
-            <input type="email" class="form-control sections" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Email">
-          </div>
+        <li class="contact-links"><a href="https://www.linkedin.com/in/charity-bunyon/" target="_blank"><i class="fab fa-linkedin contact-icons"></i>View My LinkedIn</a></li>
 
-          <div class="form-group full">
-            <textarea type="text" class="form-control sections" id="" name="message" aria-describedby="emailHelp" placeholder="Message">Message...
-            </textarea>
-          </div>
-         
-          <button type="submit" class="btn btn-primary full sections-submit">Send</button>
-        </form>
+        <li class="contact-links"><a href="https://github.com/CharityBunyon" target="_blank"><i class="fab fa-github contact-icons"></i>View My Github</a></li>
+      
       </div>
     </div>
   `;
   utilities.printToDom('contact', domString);
 };
+
+// Listen for form submit
 
 
 export default { buildContactSection };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,3 @@
-// import AOS from 'aos';
-// import 'aos/dist/aos.css';
-// import * as ScrollMagic from 'scrollmagic'; // Or use scrollmagic-with-ssr to avoid server rendering problems
-// import { TweenMax, TimelineMax } from 'gsap'; // Also works with TweenLite and TimelineLite
-// import { ScrollMagicPluginGsap } from 'scrollmagic-plugin-gsap';
 import 'jquery';
 import 'bootstrap';
 import '../styles/main.scss';
@@ -14,22 +9,6 @@ import title from './components/projectTitle/projectTitle';
 import projectDisplay from './components/projectDisplay/projectDisplay';
 import contact from './components/contact/contact';
 
-// ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
-
-// $(() => {
-// Init ScrollMagic
-// const controller = new ScrollMagic.Controller();
-
-// // build a scene
-// // eslint-disable-next-line no-unused-vars
-// new ScrollMagic.Scene({
-//   triggerElement: 'project01',
-// })
-//   .setClassToggle('#project01', 'fade-in') // add class to project01
-//   .addTo(controller);
-// });
-
-// AOS.init();
 
 const init = () => {
   navBar.buildNav();
@@ -42,20 +21,3 @@ const init = () => {
 };
 
 init();
-
-
-// const showAnimation = () => {
-//   $(document).ready(() => {
-//     const controller = new ScrollMagic.Controller();
-
-//     // build a scene
-//     // eslint-disable-next-line no-unused-vars
-//     const ourscene = new ScrollMagic.Scene({
-//       triggerElement: 'project01',
-//     })
-//       .setClassToggle('#project01', 'fade-in') // add class to project01
-//       .addTo(controller);
-//   });
-// };
-
-// showAnimation();
